66e700fbf7dbd2b9506b5997bef9c2f2dbeef8b1
Giomar Rodriguez <giorodri0584@gmail.com>
2021-04-27 18:44:38 -0400

    changes

--- app/src/main/java/com/rodriguez/giomar/el_meneo/ui/homeScreen/HomeScreen.kt
+++ app/src/main/java/com/rodriguez/giomar/el_meneo/ui/homeScreen/HomeScreen.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
@@ -39,6 +40,7 @@ class HomeScreen : Fragment() {
                         TopAppBar(title = {
                             Text(
                             "El Meneo",
+                                color = Color.White
                             )
                                           }, backgroundColor = colorResource(id = R.color.colorPrimary))
                              },
---
+++ app/src/main/java/com/rodriguez/giomar/el_meneo/ui/homeScreen/components/StoryImage.kt
@@ -0,0 +1,39 @@
+package com.rodriguez.giomar.el_meneo.ui.homeScreen.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.LinearGradient
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.glide.GlideImage
+
+@Composable
+fun StoryImage(imageUrl: String) {
+    val shape = CircleShape
+
+        Box(
+            modifier = Modifier
+                .size(70.dp)
+                .padding(3.dp)
+                .background(color = Color.LightGray, shape = shape)
+                .clip(shape)
+                .border(2.dp, Color.Gray, CircleShape)
+        ) {
+            GlideImage(
+                data = imageUrl,
+                contentDescription = "description",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+}
