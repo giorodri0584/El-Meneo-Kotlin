556df8ffcd6ba2a2b557e7a5acd69f5eb45f01cc
Giomar Rodriguez <giorodri0584@gmail.com>
2021-04-28 21:11:45 -0400

    changes

--- app/src/main/java/com/rodriguez/giomar/el_meneo/MainActivity.kt
+++ app/src/main/java/com/rodriguez/giomar/el_meneo/MainActivity.kt
@@ -3,11 +3,16 @@ package com.rodriguez.giomar.el_meneo
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
+import androidx.core.view.WindowCompat
 import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContentView(R.layout.activity_main)
         MobileAds.initialize(this)
     }
