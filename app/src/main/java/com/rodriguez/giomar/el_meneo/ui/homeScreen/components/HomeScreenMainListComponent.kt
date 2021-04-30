03e8a3d787db541bf8e58deb51efcdbed9f44e7e
Giomar Rodriguez <giorodri0584@gmail.com>
2021-04-30 19:33:44 -0400

    changes

--- app/src/main/java/com/rodriguez/giomar/el_meneo/ui/homeScreen/components/HomeScreenMainListComponent.kt
+++ app/src/main/java/com/rodriguez/giomar/el_meneo/ui/homeScreen/components/HomeScreenMainListComponent.kt
@@ -1,11 +1,15 @@
 package com.rodriguez.giomar.el_meneo.ui.homeScreen.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import com.rodriguez.giomar.el_meneo.model.YoutubeVideo
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeScreenMainListComponent (
@@ -14,10 +18,15 @@ fun HomeScreenMainListComponent (
 ){
     LazyColumn() {
         item {
-            Text(
-                text = "Historias",
-                style = MaterialTheme.typography.body1,
-            )
+            Column(
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 4.dp)
+            ) {
+                Text(
+                    text = "Historias",
+                    style = MaterialTheme.typography.body1,
+                )
+            }
+
             StoriesListComponent()
         }
         items(videos) { video ->
